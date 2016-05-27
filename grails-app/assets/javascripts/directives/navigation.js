ac69f2f755fb5cbe3146c99c9120821fc41be4d7
logan <logitimate@localhost.localdomain>
2016-05-26 23:15:16 -0400

    [logan] fixed the nav. I have the bads. Really need to get SASS working

--- grails-app/assets/javascripts/directives/navigation.js
+++ grails-app/assets/javascripts/directives/navigation.js
@@ -2,7 +2,7 @@ bowlBlitz.component('bowlBlitzNav',{
    template:
    `<nav class="navbar">
        <ul class="nav navbar-nav">
-           <li class="active"><a href="#">Bowl Blitz</a></li>
+           <li><a href="#">Bowl Blitz</a></li>
            <li><a ui-sref="scoreboard">Scoreboard</a></li>
            <li><a ui-sref="admin">Admin</a></li>
        </ul>
